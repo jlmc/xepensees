d12ea4b12a3535004d0aa5044f27888288e991e3
costajlmpp <costajlmpp@gmail.com>
2016-06-14 00:50:11 +0100

    add order verification to unit test

--- src/main/java/org/xine/xepensees/business/users/boundary/UsersManager.java
+++ src/main/java/org/xine/xepensees/business/users/boundary/UsersManager.java
@@ -10,6 +10,7 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import javax.validation.constraints.NotNull;
 
+import org.xine.xepensees.business.users.control.EncryptPassword;
 import org.xine.xepensees.business.users.control.UsersRepository;
 import org.xine.xepensees.business.users.entity.User;
 
@@ -22,9 +23,10 @@ public class UsersManager {
 	Validator validator;
 	@Inject
 	Logger logger;
+	@Inject
+	EncryptPassword encryptPassword;
 	
 	public User register(@NotNull User user) {
-		// validate
 		Set<ConstraintViolation<User>> violations = this.validator.validate(user, new Class[] {});
 		
 		if (! violations.isEmpty()) {
@@ -37,8 +39,11 @@ public class UsersManager {
 		
 		if (checkUser.isPresent()) {
 			this.logger.info("The is allready a user with the email register.");
-			throw new IllegalArgumentException("Invalid register User: " + violations);
+			throw new IllegalArgumentException("The Email '"+user.getEmail()+"' is Already in Use.");
 		}
+		
+		String digestedPassword = this.encryptPassword.digest(user.getPassword());
+		user.setPassword(digestedPassword);
 		
 		return this.usersRepository.save(user);
 	}
---
+++ src/main/java/org/xine/xepensees/business/users/control/EncryptPassword.java
@@ -0,0 +1,21 @@
+package org.xine.xepensees.business.users.control;
+
+import java.security.MessageDigest;
+import java.util.Base64;
+
+public class EncryptPassword {
+	
+	public String digest(String plainTextPassword) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(plainTextPassword.getBytes("UTF-8"));
+			byte[] passwordDigest = md.digest();
+			
+			return new String(Base64.getEncoder().encode(passwordDigest));
+			
+		} catch (Exception e) {
+			throw new RuntimeException("Exception encoding password", e);
+		}
+	}
+	
+}
