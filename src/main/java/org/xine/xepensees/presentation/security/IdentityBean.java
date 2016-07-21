a659c85eeda41e26a1210a0511264107bb3b0c11
costajlmpp <costajlmpp@gmail.com>
2016-07-21 17:21:34 +0100

    sig in refactoring

--- src/main/java/org/xine/xepensees/presentation/security/IdentityBean.java
+++ src/main/java/org/xine/xepensees/presentation/security/IdentityBean.java
@@ -2,34 +2,58 @@ package org.xine.xepensees.presentation.security;
 
 import java.io.Serializable;
 
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Instance;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 import org.xine.xepensees.business.security.entity.Identity;
+import org.xine.xepensees.presentation.faces.RedirectView;
+import org.xine.xepensees.presentation.faces.messages.Messages;
 
 @Named
-@RequestScoped
+@SessionScoped
 public class IdentityBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	// @Inject
 	private Identity identity;
 
+	@Inject
+	private Instance<Identity> identities;
+
+	@Inject
+	FacesContext facesContext;
+
+	@Inject
+	Messages messages;
+
 	public String getUsername() {
-		return String.valueOf(this.identity.getUser().getName());
+		return String.valueOf(
+				getIdentity().
+					getUser().
+						getName());
 	}
 
 	public boolean isAuthenticated() {
-		return this.identity.isAuthenticated();
+		return getIdentity().isAuthenticated();
 	}
 
 	public boolean isUser() {
-		return this.identity.isUser();
+		return getIdentity().isUser();
 	}
 
 	public boolean isAdmin() {
-		return this.identity.isAdmin();
+		return getIdentity().isAdmin();
+	}
+
+	private Identity getIdentity() {
+		if (this.identity == null) {
+			this.identity = this.identities.get();
+		}
+		return this.identity;
 	}
 
 	@Inject
@@ -38,5 +62,43 @@ public class IdentityBean implements Serializable {
 		this.identity = identity;
 	}
 
+	/**
+	 * implementation of j_security_check
+	 */
+	public RedirectView login() {
+		try {
+			final HttpServletRequest request = (HttpServletRequest) this.facesContext.getExternalContext().getRequest();
+			final String username = request.getParameter("j_username");
+			final String password = request.getParameter("j_password");
+			request.login(username, password);
+			
+			cleanIdentity();
+			
+			return RedirectView.of("/index");
+		} catch (final ServletException e) {
+			this.messages.addErrorMessageFlash("Incorrect Username or Password!");
+			return RedirectView.of("/login");
+		}
+	}
+
+	private void cleanIdentity() {
+		this.identity = null;
+	}
+
+	/**
+	 * implementation of j_security_logout
+	 */
+	public RedirectView logout() {
+		final HttpServletRequest request = (HttpServletRequest) this.facesContext.getExternalContext().getRequest();
+		try {
+			request.logout();
+
+			cleanIdentity();
+
+		} catch (final ServletException e) {
+			this.messages.addSucessMessageFlash("some problem with the logout!");
+		}
+		return RedirectView.of("/index");
+	}
 
 }
