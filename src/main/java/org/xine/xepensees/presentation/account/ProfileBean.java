68fb7f7d3e0e6b282015569524ce37a3bbafe2e3
costajlmpp <costajlmpp@gmail.com>
2016-07-22 23:01:37 +0100

    add profile page

--- src/main/java/org/xine/xepensees/presentation/account/ProfileBean.java
+++ src/main/java/org/xine/xepensees/presentation/account/ProfileBean.java
@@ -2,9 +2,12 @@ package org.xine.xepensees.presentation.account;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.xine.xepensees.business.security.entity.Identity;
 import org.xine.xepensees.business.users.entity.User;
 
 @Named
@@ -15,14 +18,18 @@ public class ProfileBean implements Serializable {
 	
 	private User user;
 	
+	@Inject
+	private Identity identity;
+
+	@PostConstruct
+	private void initialize() {
+		this.user = this.identity.getUser();
+	}
 	
 	public User getUser() {
 		return this.user;
 	}
 	
-	public void update() {
-		
-		
-	}
+
 
 }
--- src/main/resources/insert.sql
+++ src/main/resources/insert.sql
@@ -4,3 +4,17 @@ INSERT INTO T_CONFERENCE (id, version, name, country, city, date) VALUES (2002, 
 INSERT INTO T_CONFERENCE (id, version, name, country, city, date) VALUES (2003, 1, 'Devoxx MA', 'Morroco', 'Casablanca', '2016-01-01');
 INSERT INTO T_CONFERENCE (id, version, name, country, city, date) VALUES (2004, 1, 'Devoxx PL', 'Poland', 'Krakow', '2016-01-01');
 INSERT INTO T_CONFERENCE (id, version, name, country, city, date) VALUES (2005, 1, 'JavaOne', 'USA', 'San Francisco', '2016-01-01');
+
+
+INSERT INTO T_USER (email, name, password, version) VALUES('costajlmpp@gmail.com', 'Joao Costa', 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=', 1)
+INSERT INTO T_USER_ROLES (userId, permissions) VALUES('costajlmpp@gmail.com', 'ADMIN');
+INSERT INTO T_USER_ROLES (userId, permissions) VALUES('costajlmpp@gmail.com', 'USER');
+
+
+INSERT INTO T_USER (email, name, password, version) VALUES('admin@gmail.com', 'Financial Master', 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=', 1)
+INSERT INTO T_USER_ROLES (userId, permissions) VALUES('admin@gmail.com', 'ADMIN');
+
+INSERT INTO T_USER (email, name, password, version) VALUES('simplespeaker@gmail.com', 'Simple Speaker', 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=', 1)
+INSERT INTO T_USER_ROLES (userId, permissions) VALUES('simplespeaker@gmail.com', 'USER');
+
+
